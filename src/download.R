553eb01db44c0273c0fb67fcc263e549c6117b1c
David Pritchard <davidp@mentalcanvas.com>
2020-06-13 22:42:18 -0400

    Another day of Apple data.

--- src/download.R
+++ src/download.R
@@ -1,6 +1,6 @@
 # That's... one beautiful URL, Apple.
 download.file(
-  'https://covid19-static.cdn-apple.com/covid19-mobility-data/2010HotfixDev15/v3/en-us/applemobilitytrends-2020-06-11.csv',
+  'https://covid19-static.cdn-apple.com/covid19-mobility-data/2010HotfixDev16/v3/en-us/applemobilitytrends-2020-06-12.csv',
   '../input/apple.csv');
 download.file(
   'https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv',
